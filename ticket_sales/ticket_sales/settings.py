5546a44e4ef528d43545cd334afbf1c27f24e331
v_amerk <devamerkh@gmail.com>
2024-12-12 21:58:07 +0500

    docker

--- ticket_sales/ticket_sales/settings.py
+++ ticket_sales/ticket_sales/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ticket_sales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'ticket_sales_db'),
+        'USER': os.getenv('POSTGRES_USER', 'ticket_sales_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'ticket_sales_password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
